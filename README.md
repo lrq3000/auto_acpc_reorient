45c485af3a72916c29c01d8d70b3b0810779e8b4
Stephen L <lrq3000@gmail.com>
2019-03-28 14:03:29 +0100

    add: group template + increase default smoothing kernel + allow choice of smoothing kernel + update comments and doc
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # spm_auto_reorient
 
-automatic AC-PC realignment/reorientation in SPM12.
+This is a set of routines to perform "auto reorient" in the toolbox [Statistical Parametric Mapping 12 (SPM12)](https://www.fil.ion.ucl.ac.uk/spm/).
+
+## Description
+Setting up the AC-PC and reorienting images is a recurrent issue in between-subjects group analyses, since they rely on coregistration methods that, like the "unified segmentation" of SPM12, are for most sensitive to initial conditions (the starting orientation of the image). This routine is a somewhat enhanced version of the original code by [John Ashburner](https://en.wikibooks.org/wiki/SPM/How-to#How_to_automatically_reorient_images?).
+
+The main function, `spm_auto_reorient.m`, automatically (but approximately) calculates a non-linear coregistration of the input image onto a target template in MNI space, and then applies only the rigid-body transform part of this coregistration to reorient the input image. This allows to mainly set the AC location and correct for head rotation, in order to further proceed with the segmentation/normalisation of the image. This relies on a "template matching" principle (as in the old normalize function), you therefore ought to specify the appropriate template/reference image (we provide some).
+
+In any case, it is advised to check the automatically reoriented images, and [fix the orientation manually (SPM -> Display)](https://en.wikibooks.org/wiki/SPM/How-to#How_to_manually_change_the_orientation_of_an_image?) if necessary.
+
+## Install
+
+To install this tool :
+* copy `spm_auto_reorient.m` in your `spm` folder. This will allow the command `spm_auto_reorient()` to be called from command-line (if no argument is given, a file selector dialog will open).
+* copy `T1_template_CAT12_rm_withskull.nii` to your `spm/canonical` folder. This is a template generated on 10 subjects using CAT12 that were manually reoriented to AC-PC, this provides a better performance.
+
+The tool can be included in the batching system of SPM12 by : 
+- copying the `spm_cfg_autoreorient.m` file to the `spm/config` sub-directory. Then this module can be included in any processing pipeline;
+- optionally, if you want an easy access from the SPM's spatial pull-down menu directly, by overwriting the `spm_cfg.m` with the one provided here (BEWARE: this was not updated since a long time, this may break down your SPM install!).
+
+## Usage
+
+Type `help spm_auto_reorient`, for all the details and various options.
+
+## Guarantee
+There is no guarantee that this will work 100% of the times, although it was observed to produce good results with our own data (young and old healthy subjects, AD/PD patients, most of brain damaged patients even with significant movement artefacts).
+
+For a comparison of various methods for AC-PC reorientation, the following article is a good read:
+
+`Liu, Yuan, and Benoit M. Dawant. "Automatic detection of the anterior and posterior commissures on MRI scans using regression forests." 2014 36th Annual International Conference of the IEEE Engineering in Medicine and Biology Society. IEEE, 2014.`
+
+## Authors
+The code was originally written by John Ashburner & Carlton Chu (FIL, UCL, London, UK). Additional modifications were made by Stephen Karl Larroque (Coma Science Group, GIGA-Consciousness, University of Liege, Belgium).
+
+## License
+General Public License (GPL) v2
+
--- spm_auto_reorient.m
+++ spm_auto_reorient.m
@@ -1,21 +1,105 @@
-function auto_reorient(p) 
-spmDir=which('spm');
-spmDir=spmDir(1:end-5);
-tmpl=[spmDir 'canonical\avg152T1.nii'];
-vg=spm_vol(tmpl);
-flags.regtype='rigid';
-%p=spm_select(inf,'image');
-for i=1:size(p,1)
-    f=strtrim(p(i,:));
-    spm_smooth(f,'temp.nii',[12 12 12]);
-    vf=spm_vol('temp.nii');
-    [M,scal] = spm_affreg(vg,vf,flags);
-    M3=M(1:3,1:3);
-    [u s v]=svd(M3);
-    M3=u*v';
-    M(1:3,1:3)=M3;
-    N=nifti(f);
-    N.mat=M*N.mat;
+function spm_auto_reorient(p,img_type,smooth_factor)
+
+% FORMAT spm_auto_reorient(p,img_type,smooth_factor)
+% Automatically reorient a T1 image (or any other usual image modality) in the MNI space
+% This uses a non-linear coregistration on a template,
+% although the reorientation only applies a rigid-body transform.
+%
+% This is useful as group analysis rely on between-subjects coregistration
+% which is for most methods, such as in "unified segmentation", sensitive
+% to initial conditions (the starting orientation of the image).
+%
+% It is advised to check (and fix if necessary) manually the result.
+%
+% - p       : filepath of NIfTI image to reorient (as `ls` returns).
+% - img_type    : template image type 'T1group' (default), 'T1canonical', 'T1', 'T2', 'PET', 'EPI',...
+%               i.e. any of the templates provided by SPM/canonical. 'T1group' is a custom template we provide,
+%               computed as the average T1 from normalized T1 images from 10 subjects with intensity normalization
+%               and without skull stripping using CAT12 (rm* file). 'T1canonical' is the T1 brain image of a single subject
+%               as provided by SPM. Note that you can adjust the respective template if you want to tweak
+%               the target realignment to your liking. Special option: can also use the path to a nifti file
+%               (this allows for cross-modality coregistration, no smoothing is applied here then, in this case
+%               it is advised to use mode 'mi' only).
+% - smooth_factor : smoothing kernel (isotropic). Default: 20. Usually, a big kernel helps in coregistering
+%             to template, particularly for brain damaged patients, but might also help with healthy volunteers.
+%             However, a too big kernel will also result in suboptimal coregistration. 20 is good for T1.
+%
+% Returns: nothing, but the input image's headers are modified.
+%__________________________________________________________________________
+
+% Code originally written by John Ashburner & Carlton Chu, FIL, UCL, London, UK
+% Extended by Stephen Karl Larroque, Coma Science Group & GIGA-Consciousness, University Hospital of Liege, Belgium
+
+% Init input variables
+if ~exist('p', 'var')
+    p = spm_select(inf, 'image', 'Select nifti files to reorient');
+end
+p = char(p);
+imgcount = size(p,1);
+
+if ~exist('img_type', 'var')
+    img_type = 't1group';
+end
+img_type = char(img_type);
+if size(img_type, 1) ~= imgcount
+    error('Invalid number of template images, the number template does not match the number of input images (if you want to apply one different template for each input image, else you can just use one template for all reorientations).');
+end
+
+
+if ~exist('smooth_factor', 'var')
+    smooth_factor = 20;
+end
+
+%% Select template to reorient to
+img_type = lower(img_type);
+% Manage special cases (ie, template name is too long so we allow a shorthand)
+if strcmp(img_type, 't1canonical')
+    img_template = fullfile(spmDir, 'canonical', 'single_subj_T1.nii')
+elseif strcmp(img_type, 't1group')
+    img_template = fullfile(spmDir, 'canonical', 'T1_template_CAT12_rm_withskull.nii');  % you need to add this file into spm/canonical
+    if ~exist(img_template, 'file') == 2  % if template cannot be found in spm folder, try to look locally, in same folder as current script
+        % Build the path to current script (because pwd is unreliable)
+        scriptpath = mfilename('fullpath');
+        scriptdir = fileparts(scriptpath); % get the parent directory of the current script
+        img_template = fullfile(scriptdir, 'T1_template_CAT12_rm_withskull.nii');  % this file needs to be in the same folder as this script
+        if ~exist(img_template, 'file') == 2
+            error('Cannot find template t1group, please make sure the nifti file is at the appropriate place (see readme)')
+        end %endif
+    end %endif
+elseif exist(img_type(1,:), 'file') == 2
+    % The template is directly a file (or list of files), we use it as a template (can be used to coregister across modalities, eg EPI BOLD on T1)
+    % Note that then we will use img_type as a cellarr of filepaths to the templates to use
+    img_template = 'file';
+else
+    img_template = fullfile(spmDir, 'toolbox', 'OldNorm', [upper(img_type) '.nii']);
+    % if template cannot be found in spm folder, try to use the provided img_type as a filepath
+    if exist(img_template, 'file') ~= 2
+        error('Cannot find template %s, please make sure the nifti file can be found either in SPM/toolbox/OldNorm or you can provide a full filepath.', img_template)
+    end %endif
+end %endif
+vg = spm_vol(img_template);  % get template image
+
+% Configure coregistration to template (will be the basis of the reorientation)
+flags.sep = 5;  % sampling distance. Reducing this enhances a bit the reorientation but significantly increases processing time.
+flags.regtype = 'mni';  % can be 'none', 'rigid', 'subj' or 'mni'. On brain damaged patients, 'mni' seems to give the best results (non-affine transform), but we don't use the scaling factor anyway. See also a comparison in: Liu, Yuan, and Benoit M. Dawant. "Automatic detection of the anterior and posterior commissures on MRI scans using regression forests." 2014 36th Annual International Conference of the IEEE Engineering in Medicine and Biology Society. IEEE, 2014.
+
+% For each input image
+for i = 1:imgcount
+    % get image, create a temporary file (to avoid permission issues) and smooth to ease coregistration to template
+    f = strtrim(p(i,:));
+    spm_smooth(f,'temp.nii',[smooth_factor smooth_factor smooth_factor]);
+    vf = spm_vol('temp.nii');
+    % calculate the reorientation matrix from source image to match template image
+    [M, scal] = spm_affreg(vg,vf,flags);
+    M3 = M(1:3,1:3);
+    [u s v] = svd(M3);
+    M3 = u*v';
+    M(1:3,1:3) = M3;
+    % reload source image to apply the transform on it
+    N = nifti(f);
+    N.mat = M*N.mat;
     create(N);
+end
+
 end
 
